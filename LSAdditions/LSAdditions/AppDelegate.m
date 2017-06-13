acf537235200220b533398203bf9fba92ee59f1e
LSRain <315192042@qq.com>
2017-06-13 11:03:51 +0800

    delete IB & appoint rootVC

--- LSAdditions/LSAdditions/AppDelegate.m
+++ LSAdditions/LSAdditions/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,12 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    _window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    ViewController *vc = [ViewController new];
+    _window.rootViewController = vc;
+    [_window makeKeyAndVisible];
+    
     return YES;
 }
 
