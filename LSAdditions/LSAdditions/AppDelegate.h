ef906b9b686bce108da61e7eb8ec405f6462915d
LSRain <315192042@qq.com>
2017-06-13 11:00:49 +0800

    Project init

---
+++ LSAdditions/LSAdditions/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LSAdditions
+//
+//  Created by WangBiao on 2017/6/13.
+//  Copyright © 2017年 LSRain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
